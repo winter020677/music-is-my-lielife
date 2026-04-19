b19b65ef4651c2b702285e80f2f0070f6c36ad53
winter020677 <riri0206riri0@gmail.com>
2026-04-19 18:34:15 +0900

    add frontend titlw

---
+++ frontend/app/components/SongForm.tsx
@@ -0,0 +1,7 @@
+ export default function SongForm() {
+    return (
+      <form>
+        <input type="text" placeholder="曲名" />
+      </form>
+    )
+  }
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,12 @@
-export default function Home() {
+import SongForm from "./components/SongForm"
+
+  export default function Home() {
     return (
-      <main>
-        <h1>Music is my lielife</h1>
+      <main className="min-h-screen bg-zinc-950 p-8">
+        <h1 className="text-3xl font-bold text-white">
+          Music is my lielife
+        </h1>
+        <SongForm/>
       </main>
     )
   }
